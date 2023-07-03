5b4ed5dac7b34a1b33a5205e9971c5d74610d5e4
Swaroop Sethumadhavan <swaroop.striker@gmail.com>
2023-07-03 17:31:45 +0200

    Add test for name step

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
+test('renders get started link', () => {
   render(<App />)
   const linkElement = screen.getByText(/Get started/i)
   expect(linkElement).toBeInTheDocument()
